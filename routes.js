6b83a0534103fab5d470ab558a1fe3c843ad73dc
Dote <zoqol@daum.net>
2023-03-06 17:49:45 +0900

    study-mock

--- routes.js
+++ routes.js
@@ -4,4 +4,6 @@ const productController = require("./controller/products");
 
 router.get("/one", productController.hello);
 
+router.post("", productController.createProduct);
+
 module.exports = router;
--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -1,3 +1,33 @@
-test("tow plus two is four", () => {
-  expect(2 + 2).toBe(4);
+const productController = require("../../controller/products");
+const productModel = require("../../models/Product");
+const httpMocks = require("node-mocks-http");
+const mockProduct = require("../data/mock-product.json");
+
+productModel.create = jest.fn();
+
+let req, res, next;
+beforeEach(() => {
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = null;
+});
+
+describe("Product Controller Create", () => {
+  beforeEach(() => {
+    req.body = mockProduct;
+  });
+  it("should have a createProduct function", () => {
+    expect(typeof productController.createProduct).toBe("function");
+  });
+
+  it("should call ProductModel.create", () => {
+    productController.createProduct(req, res, next);
+    expect(productModel.create).toBeCalledWith(mockProduct);
+  });
+
+  it("should return 201 response code", () => {
+    productController.createProduct(req, res, next);
+    expect(res.statusCode).toBe(201);
+    expect(res._isEndCalled()).toBeTruthy();
+  });
 });
