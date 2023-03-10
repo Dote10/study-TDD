c5f0ce544e2b44958c5bdbe7264ac611f7edb99f
Dote <zoqol@daum.net>
2023-03-10 13:12:37 +0900

    study Jest mockFn

--- test/unit/foo.test.js
+++ test/unit/foo.test.js
@@ -73,7 +73,7 @@ describe("Mock Return This", () => {
 describe("Mock Name", () => {
   test("test display mock function name", () => {
     const myMockFn = jest.fn().mockName("namedMockFn");
-    //myMockFn();
+    myMockFn();
     expect(myMockFn).toHaveBeenCalled(); //호출이되는지 테스트
   });
 });
@@ -83,3 +83,45 @@ describe("Mock Name", () => {
 
 // 이름 지정 한 경우
 // Error: expect(namedMockFn).toHaveBeenCalled()
+
+//Custom Matchers
+describe("Custom Matcher", () => {
+  const mockFunc = jest.fn();
+  const arg1 = 1;
+  const arg2 = 2;
+
+  test("The mock function was called at least once", () => {
+    mockFunc();
+    expect(mockFunc).toHaveBeenCalled();
+    //아래 코드와 동일하게 동작
+    //expect(mockFunc.mock.calls.length).toBeGreaterThan(0)
+  });
+
+  //어떤 인자와 함께 불렸는지 알 수있게 해준다.
+  test("The mock function was called at least once with the specified args", () => {
+    mockFunc(arg1, arg2);
+    expect(mockFunc).toHaveBeenCalledWith(arg1, arg2);
+    //아래 코드와 동일하게 동작
+    //expect(mockFunc.mock.calls).toContainEqual([arg1,arg2]);
+  });
+
+  //특정 함수가 마지막에 불려질때 어떤 인자와 함께 불려졌는지
+  test("The last call to the mock function was called with the specified args", () => {
+    mockFunc(3, 4);
+    mockFunc(arg1, arg2);
+    expect(mockFunc).toHaveBeenLastCalledWith(arg1, arg2);
+    //아래 코드와 동일하게 동작
+    //expect(mockFunc.mock.calls[mockFunc.mock.calls.length - 1 ]).toEqual([
+    // arg1,
+    // arg2
+    //]);
+  });
+
+  test("All calls and the name of the mock is written as a snapshot", () => {
+    mockFunc(arg1, arg2);
+    expect(mockFunc).toMatchSnapshot();
+    //아래 코드와 동일하게 동작
+    //expect(mockFunc.mock.calls).toEqual([[arg1,arg2]]);
+    //expect(mockFunc.getMockName()).toBe('a mock name');
+  });
+});
